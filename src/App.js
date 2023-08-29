a2e2d4ed6392a10ad621d1f40bca5a2c52edb2a8
nm1008 <mallari08nikko@yahoo.com>
2023-08-29 08:01:02 +0800

    built a date counter that uses useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
     </div>
   );
 }
 
-export default App;
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  function minusCount() {
+    setCount((c) => c - step);
+  }
+
+  function addCount() {
+    setCount((c) => c + step);
+  }
+
+  function minusStep() {
+    if (step > 1) setStep((s) => s - 1);
+  }
+
+  function addStep() {
+    setStep((s) => s + 1);
+  }
+
+  function calculateDate() {
+    const today = new Date();
+    console.log(today.getFullYear())
+    const targetDate = new Date();
+    targetDate.setDate(today.getDate() + count);
+    
+    return targetDate.toDateString();
+  }
+
+  function getDisplayMessage() {
+    if (count > 0) {
+      return `${count} days from today is ${calculateDate()}`;
+    } else if (count < 0) {
+      return `${-count} days ago was ${calculateDate()}`;
+    } else {
+      return `Today is ${calculateDate()}`;
+    }
+  }
+  function reset() {
+    setCount(0);
+  }
+
+  const displayMessage = getDisplayMessage();
+
+  return (
+    <>
+      <div>
+        <button onClick={minusStep}>-</button>
+        <span>Step: {step}</span>
+        <button onClick={addStep}>+</button>
+      </div>
+
+      <div>
+        <button onClick={minusCount}>-</button>
+        <span>Count: {count}</span>
+        <button onClick={addCount}>+</button>
+      </div>
+
+      <div>{displayMessage}</div>
+
+      <div>
+        <button onClick={reset}>Reset</button>
+      </div>
+    </>
+  );
+}
